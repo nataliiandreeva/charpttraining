41f575ae08ef089e09d45eb223f6c52394443632
nataliiandreeva <nandreeva797@gmail.com>
2021-05-19 21:11:42 +0300

    Проект создан

---
+++ addressbook-web-testes/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
