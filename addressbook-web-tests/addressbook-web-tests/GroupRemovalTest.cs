c396fba1568a4b7b530298c84ec07ba835b838f3
nataliiandreeva <nandreeva797@gmail.com>
2021-05-30 16:24:34 +0300

    Create base test class

---
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {       
+        [Test]
+        public void TheBookManyHealthcareAppointmentsTest()
+        {
+            GoToHomePage();
+            loginHelper.Login(new AccountData("admin", "secret"));
+            GoToGroupPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupPage();
+        }
+    }
+}
